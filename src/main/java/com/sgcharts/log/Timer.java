e2b11550a72ddb3bb467e272566d672f6c6c952a
Ruhong <seahrh@gmail.com>
2018-01-20 06:11:15 +0800

    first commit

---
+++ src/main/java/com/sgcharts/log/Timer.java
@@ -0,0 +1,24 @@
+package com.sgcharts.log;
+
+import java.time.Duration;
+
+/**
+ * Accurate measure of time elapsed, usually used to profile code execution.
+ * Based on <a href="https://stackoverflow.com/a/1770038/519951">https://stackoverflow.com/a/1770038/519951</a>
+ */
+public class Timer {
+    private long start;
+
+    public static Timer start() {
+        return new Timer();
+    }
+
+    private Timer() {
+        start = System.nanoTime();
+    }
+
+    public Duration time() {
+        long end = System.nanoTime();
+        return Duration.ofNanos(end - start);
+    }
+}
